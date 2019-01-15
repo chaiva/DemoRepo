1b67210214e01e92c68e966274ffb1d23e3248ff
chaiva <ns.chaithra@gmail.com>
2019-01-14 21:26:18 -0800

    First Commit

--- src/main/java/com/qa/util/TestUtil.java
+++ src/main/java/com/qa/util/TestUtil.java
@@ -1,5 +1,13 @@
 package com.qa.util;
 
+import org.testng.annotations.Test;
+
 public class TestUtil {
+	
+@Test
+public void TestUtil()
+{
+	System.out.println("!!!!!!!!Test Util!!!!!!!!!!!!!!");
+}
 
 }
